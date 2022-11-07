d7422b4342b0432a8d9d09d26cf677436bc9cdca
AdrienD <adrien.deghilage@live.fr>
2022-11-07 12:20:38 +0100

    -all

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,6 +2,49 @@ import "../Styles/Cart.css";
 import { useState } from "react";
 
 function Cart({ cart, updateCart }) {
+  /* Bouton + */
+  function addCurrentPlant(name, price) {
+    const currentPlant = cart.find((plant) => plant.name === name);
+    const cartBeforeCurrentPlant = cart.filter(
+      (plant) => plant.name < currentPlant.name
+    );
+    const cartAfterCurrentPlant = cart.filter(
+      (plant) => plant.name > currentPlant.name
+    );
+    updateCart([
+      ...cartBeforeCurrentPlant,
+      {
+        name,
+        price,
+        amount: currentPlant.amount + 1,
+      },
+      ...cartAfterCurrentPlant,
+    ]);
+  }
+
+  /* Bouton - */
+  function delCurrentPlant(name, price) {
+    const currentPlant = cart.find((plant) => plant.name === name);
+    const cartBeforeCurrentPlant = cart.filter((plant) => plant < currentPlant);
+    const cartAfterCurrentPlant = cart.filter((plant) => plant > currentPlant);
+    const cartFilteredCurrentPlant = cart.filter(
+      (plant) => plant.name !== name
+    );
+    if (currentPlant.amount <= 0) {
+      updateCart([...cartFilteredCurrentPlant]);
+    } else {
+      updateCart([
+        ...cartBeforeCurrentPlant,
+        {
+          name,
+          price,
+          amount: currentPlant.amount - 1,
+        },
+        ...cartAfterCurrentPlant,
+      ]);
+    }
+  }
+
   const [isOpen, setIsOpen] = useState(false);
   const total = cart.reduce(
     (acc, plantType) => acc + plantType.amount * plantType.price,
@@ -13,8 +56,15 @@ function Cart({ cart, updateCart }) {
       <button onClick={() => setIsOpen(false)}>Fermer</button>
       <h2>Panier</h2>
       {cart.map(({ name, price, amount }, index) => (
-        <div key={`${name}-${index}`}>
-          {name} {price}€ x {amount}
+        <div className="lmj-cart-plant" key={`${name}-${index}`}>
+          <div className="lmj-cart-plantnameprice">
+            {name} {price}€
+          </div>
+          <div className="lmj-cart-amount">
+            <button onClick={() => addCurrentPlant(name, price)}>+</button>{" "}
+            {amount}
+            <button onClick={() => delCurrentPlant(name, price)}>-</button>
+          </div>
         </div>
       ))}
       <h3>Total : {total}€</h3>
